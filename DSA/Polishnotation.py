d0b169adb4085a50ab4e3940e9d20cc4629970d0
krishwall <krishjaiswal523@gmail.com>
2024-06-18 10:51:19 +0530

    # 11map,filter,*args,**kwargs

--- DSA/Polishnotation.py
+++ DSA/Polishnotation.py
@@ -6,25 +6,46 @@ def polish(exp):
     list1.append(')')
     nexp=[]
     stack=[]
-    for i in range(len(list1)):
-        if(list1[i].isalpha()!=True)       :
-            stack.append(list1[i])     
-        elif(list1[i]==')'):
-            for j in range(i,0,-1):
-                print(i)
-                if(list1[j]=='('):
-                     nexp.append(stack.pop())
-                     break    
+    for i in range(0,len(list1)):
+        print(i) 
+        k=len(stack)
+        if(list1[i]==')'):
+            print(' ) encountered')
+            if len(stack)>0:
+              while(stack[-1]!='('):
+                print('-----------------------') 
+            
+                nexp.append(stack.pop())
+                k-=1
                 
-                      
-        else: 
-            nexp.append(list1[i])
+                print(stack)
+                print(nexp)       
+                if len(stack)<=1:
+                    break
+              if stack[-1]=='(':
+                  stack.pop()
+        elif(list1[i].isalpha()==True)       :
+            nexp.append(list1[i])    
+        elif len(stack)>0:
+
+            if (stack[-1]=='*' or stack[-1]=='/') and (list1[i]=='+' or list1[i]=='-'):
+
+              print("here")
+
+              nexp.append(stack.pop())
+            stack.append(list1[i])  
+        else :
+             stack.append(list1[i])   
+        print(list1) 
+        print(stack)
+        print(nexp)
+       
+       
+        
     print(list1)
     print(stack)
     print(nexp)
+    return(' '.join(nexp))
 
+print(polish('(A*(B+((C+D)*(E+F))/G))*H'))
 
-polish('A+(B*C-(D/E!F)*G)*H')
-
-
- 
