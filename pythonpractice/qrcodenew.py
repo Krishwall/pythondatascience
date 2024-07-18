d9dd3f67b3e3d4e854aea105ac7a9799e6d73e59
krishwall <krishjaiswal523@gmail.com>
2024-07-18 14:17:11 +0530

    #25 open cv , image processing ,videos frames

--- pythonpractice/qrcodenew.py
+++
@@ -1,4 +0,0 @@
-import segno
-qrcode=segno.make("https://www.pinterest.com/pin/hehe-in-2023--63402307247865935/")
-
-qrcode.save("lightblue_qrcode.png",scale=5,light="lightblue")
--- pythonpractice/qrcodeurl1.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-data="file:///D:/pythondatascience/NEWFILE.html"
-qr=qrcode.QRCode(version=1,
-                 box_size=10,
-                 border=5)
-qr.add_data(data)
-qr.make(fit=True)
-img=qr.make_image()
-img.save('MYQRCode2.png')
